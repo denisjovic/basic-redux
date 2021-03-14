f6479364a013e4149ec438020b07d932d02de1c4
denisjovic <denis.jovic@tn-tech.co.rs>
2021-03-14 14:51:21 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  incrementAction,
+  decrementAction,
+  logInAction,
+  logOutAction,
+} from './actions';
 
 function App() {
+  // extract the desired part of state from the global state
+  const counter = useSelector((state) => state.CounterReducer);
+  const loggedIn = useSelector((state) => state.loggedReducer);
+
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='logIn'>
+        <button onClick={() => dispatch(logInAction())}>Log in</button>
+        <button onClick={() => dispatch(logOutAction())}>Log out</button>
+      </div>
+      <div className='counter'>
+        <button onClick={() => dispatch(incrementAction())}>+</button>
+        <button onClick={() => dispatch(decrementAction())}>-</button>
+        <h2>Counter {counter}</h2>
+      </div>
+      {loggedIn ? (
+        <h1>Some sensitive data</h1>
+      ) : (
+        <h1>Nothing to see, move along</h1>
+      )}
     </div>
   );
 }
---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+export const incrementAction = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+
+export const decrementAction = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+
+export const logInAction = () => {
+  return {
+    type: 'SIGN_IN',
+  };
+};
+
+export const logOutAction = () => {
+  return {
+    type: 'SIGN_OUT',
+  };
+};
